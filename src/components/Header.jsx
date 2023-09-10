eba00657fc3b0f8b4a00388e4f80973c81083fb7
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-09-10 16:03:52 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <>
+      <nav>
+        <div>
+          <h1>Knowledge Cafe</h1>
+        </div>
+        <div>
+          <img src="../../public/images/Ellipse 1.png" alt="logo image" />
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
